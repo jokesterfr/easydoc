e327a48b7bec8f13fc5cd8636615f0088fceaf9a
Clément <clement.desiles@telecomsante.com>
2013-03-13 16:03:38 +0100

    Enhance style for $name$ h1

--- public/build/templates.js
+++ public/build/templates.js
@@ -87,8 +87,17 @@ function program4(depth0,data) {
   return buffer;});
 templates['page'] = template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers; data = data || {};
-  var buffer = "", stack1, foundHelper, functionType="function";
+  var buffer = "", stack1, stack2, foundHelper, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = "", stack1, foundHelper;
+  buffer += "\n		<h1 class=\"doc-title\">";
+  foundHelper = helpers.title;
+  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1) + "</h1>\n	";
+  return buffer;}
 
   buffer += "<nav id=\"table-of-contents\">\n	";
   foundHelper = helpers.toc;
@@ -96,6 +105,11 @@ templates['page'] = template(function (Handlebars,depth0,helpers,partials,data) 
   else { stack1 = depth0.toc; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n</nav>\n\n<article id=\"document\">\n	";
+  stack1 = depth0.title;
+  stack2 = {};
+  stack1 = helpers['if'].call(depth0, stack1, {hash:stack2,inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n	";
   foundHelper = helpers.body;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.body; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
--- public/tpl/page.tpl
+++ public/tpl/page.tpl
@@ -3,5 +3,8 @@
 </nav>
 
 <article id="document">
+	{{#if title}}
+		<h1 class="doc-title">{{title}}</h1>
+	{{/if}}
 	{{{body}}}
 </article>
